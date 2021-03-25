a3df9b1b1e7944bdaf910ad52ae3e810bb122257
radex <radexpl@gmail.com>
2021-03-25 12:18:42 +0100

    Update dependencies & fix M1 workflow

--- dist/with-observables.esm.js
+++ dist/with-observables.esm.js
@@ -4,7 +4,17 @@ import hoistNonReactStatic from 'hoist-non-react-statics';
 function _inheritsLoose(subClass, superClass) {
   subClass.prototype = Object.create(superClass.prototype);
   subClass.prototype.constructor = subClass;
-  subClass.__proto__ = superClass;
+
+  _setPrototypeOf(subClass, superClass);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+    o.__proto__ = p;
+    return o;
+  };
+
+  return _setPrototypeOf(o, p);
 }
 
 var cleanUpBatchingInterval = 250; // ms
@@ -103,9 +113,7 @@ function getTriggeringProps(props, propNames) {
 var hasOwn = Object.prototype.hasOwnProperty; // TODO: This is probably not going to be 100% safe to use under React async mode
 // Do more research
 
-var WithObservablesComponent =
-/*#__PURE__*/
-function (_Component) {
+var WithObservablesComponent = /*#__PURE__*/function (_Component) {
   _inheritsLoose(WithObservablesComponent, _Component);
 
   function WithObservablesComponent(props, BaseComponent, getObservables, triggerProps) {
@@ -332,9 +340,7 @@ function (_Component) {
 
 var withObservables = function withObservables(triggerProps, getObservables) {
   return function (BaseComponent) {
-    var ConcreteWithObservablesComponent =
-    /*#__PURE__*/
-    function (_WithObservablesCompo) {
+    var ConcreteWithObservablesComponent = /*#__PURE__*/function (_WithObservablesCompo) {
       _inheritsLoose(ConcreteWithObservablesComponent, _WithObservablesCompo);
 
       function ConcreteWithObservablesComponent(props) {
